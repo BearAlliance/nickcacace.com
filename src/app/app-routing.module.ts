1690ad093cdfbafabb4aaaae3c5c3d685970ffe9
Nick Cacace <nick.cacace@adp.com>
2018-11-12 19:46:12 -0500

    chore: update to Angular@7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,11 +24,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(
-      appRoutes
-    )
-  ],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
