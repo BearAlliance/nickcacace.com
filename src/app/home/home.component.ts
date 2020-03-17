51ef96fbbb2354d6adf756ca427351513993bfa0
Nick Cacace <NickCacace@gmail.com>
2020-03-17 16:21:25 -0400

    feat(home): update landing page text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { IProject, ProjectsConstant } from '../shared/projects.constant';
 })
 export class HomeComponent {
   recentWork: IProject[];
-  landingText = `My name's Nick. I'm a software developer with a passion for pushing boundaries on the trail, and on the web. I've worked with a variety of leading technologies to create beautiful progressive web apps, static sites, internal dev tools, and back-end systems. When I'm not coding I spend my time hiking, cycling, or building things out of wood. Most recently living and working in New York City, I've been working for ADP on the MyADP project to unify core employee and manager services for thousands of enterprise HR and payroll clients across the country, and the world. I can't share any of that code with you, but check out some of my side projects!`;
+  landingText = `My name's Nick. I'm a software developer with a passion for pushing boundaries on the trail, and on the web. I've worked with a variety of leading technologies to create beautiful progressive web apps, static sites, internal dev tools, and back-end systems. When I'm not coding I spend my time hiking, cycling, or building things out of wood. I live in upstate New York, and work remotely for American Express. I'm creating loan products for individuals and businesses across the country, and the world. I can't share any of that code with you, but check out some of my side projects!`;
 
   constructor() {
     this.recentWork = [
