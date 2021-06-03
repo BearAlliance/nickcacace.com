9d8c158403ad25c2b1036f2181ff351da565a882
Nick Cacace <NickCacace@gmail.com>
2021-06-03 16:58:05 -0400

    chore: upgrade angular to version 12

--- src/app/image-modal/image-modal.component.spec.ts
+++ src/app/image-modal/image-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImageModalComponent } from './image-modal.component';
 
@@ -6,11 +6,13 @@ describe('ImageModalComponent', () => {
   let component: ImageModalComponent;
   let fixture: ComponentFixture<ImageModalComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ImageModalComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ImageModalComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ImageModalComponent);
--- src/app/top-nav/top-nav.component.spec.ts
+++ src/app/top-nav/top-nav.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TopNavComponent } from './top-nav.component';
 
@@ -6,11 +6,13 @@ describe('TopNavComponent', () => {
   let component: TopNavComponent;
   let fixture: ComponentFixture<TopNavComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TopNavComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TopNavComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TopNavComponent);
